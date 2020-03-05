ea1c89be66012759b9c69cf1d6884ae477aeb383
Felix Nogales <felixnoga@gmail.com>
2020-03-05 19:10:32 +0100

    Ended

--- routes/booking.routes.js
+++ routes/booking.routes.js
@@ -3,23 +3,23 @@ const express = require('express')
 const router = express.Router()
 const bookingController = require('../controllers/booking.controller')
 const { isAuthenticated } = require('../middlewares/isAuthenticated')
+const { isFakeAuthenticated } = require('../middlewares/isFakeAuthenticated')
 
-
-router.get('/', [isAuthenticated], bookingController.getCurrentUserBooking)
-router.put('/', [isAuthenticated], bookingController.createCurrentUserBooking)
+router.get('/', [isFakeAuthenticated], bookingController.getCurrentUserBooking)
+router.put('/', [isFakeAuthenticated], bookingController.createCurrentUserBooking)
 router.get('/Keeper', [isAuthenticated], bookingController.getCurrentKeeperBooking)
 
 /*
 router.get('/user/:userId', [isAuthenticated], userController.getUserById)
 
 
-router.get('/booking',[isAuthenticated], bookingController.retrieve)       
-router.post('/booking', [isAuthenticated], bookingController.create) 
-router.get('/booking/:bookingId', [isAuthenticated], bookingController.get) 
-router.put('/booking/:bookingId', [isAuthenticated], bookingController.update) 
-router.patch('/booking/:bookingId/accept', [isAuthenticated], bookingController.accept) 
-router.delete('/booking/:bookingId',[isAuthenticated], bookingController.delete) 
-router.delete('booking/:bookingId/cancel', [isAuthenticated], bookingController.cancel) 
+router.get('/booking',[isAuthenticated], bookingController.retrieve)
+router.post('/booking', [isAuthenticated], bookingController.create)
+router.get('/booking/:bookingId', [isAuthenticated], bookingController.get)
+router.put('/booking/:bookingId', [isAuthenticated], bookingController.update)
+router.patch('/booking/:bookingId/accept', [isAuthenticated], bookingController.accept)
+router.delete('/booking/:bookingId',[isAuthenticated], bookingController.delete)
+router.delete('booking/:bookingId/cancel', [isAuthenticated], bookingController.cancel)
 */
 
 module.exports = router
--- routes/site.routes.js
+++ routes/site.routes.js
@@ -2,12 +2,13 @@ const express = require('express')
 
 const siteController = require('../controllers/site.controller')
 const { isAuthenticated } = require('../middlewares/isAuthenticated')
+const { isFakeAuthenticated} = require('../middlewares/isFakeAuthenticated')
 
 const router = express.Router()
 
 router.put('/', [isAuthenticated], siteController.createCurrentUserSite)
 router.get('/', [isAuthenticated], siteController.getCurrentUserSite)
-router.get('/All', [isAuthenticated], siteController.getAllUsersSite)
+router.get('/All', [isFakeAuthenticated], siteController.getAllUsersSite)
 router.put('/:site', [isAuthenticated], siteController.updateSiteById)
 
 /*
